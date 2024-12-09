53e0b0391369e5aa92f16b88a11179fa8cb10b74
zsmoore <zsmoore@zachary-moore.com>
2024-12-08 23:06:50 -0500

    day 1 part2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod day_1;
+mod util;
 
 fn main() {
     // day 1
-    solve_day_1()
+    solve_day_1();
 }
 
 fn solve_day_1() {
     println!("Day 1 solution: {}", day_1::solution::solve_input());
+    println!("Day 1 solution part 2: {}", day_1::solution::solve_input_part_2());
 }
---
+++ src/util/utils.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+/// Load 2 lists of numbers from file
+pub fn load_input(file_path: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut input_1 = Vec::new();
+    let mut input_2 = Vec::new();
+
+    if let Ok(lines) = read_lines(file_path) {
+        for line in lines.flatten() {
+            let mut split = line.split_whitespace();
+            input_1.push(split.next().unwrap().parse().unwrap());
+            input_2.push(split.next().unwrap().parse().unwrap());
+        }
+    }
+
+    (input_1, input_2)
+}
+
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
+}
